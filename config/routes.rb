9d8859253af6870b19550f74699df5972ba16b93
Diego Fonseca <dfonseca@eafit.edu.co>
2018-07-21 14:03:49 -0500

    store position db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'event/get_location'
+  get 'event/store'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
