20d55200d6ad727c7c98c29d3023b785c3b6f69f
root <root@ip-172-31-17-167.us-east-2.compute.internal>
2018-07-31 21:42:02 +0000

    api

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get 'event/store'
   get 'event/get_date'
   root :to =>'home#index'
+  namespace :api, default: {format: 'json'} do
 
+    resources :location, only: [:create]
+    
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
